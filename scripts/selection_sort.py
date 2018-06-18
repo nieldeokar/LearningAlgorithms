c7328e198f34afe6fc6e476837f631816fb928ea
Nilesh deokar <wilson@porsh.net>
2018-06-18 14:56:05 +0200

    binary & selection sort

---
+++ scripts/selection_sort.py
@@ -0,0 +1,25 @@
+import sys
+import subprocess
+import re
+
+
+def ndSmallest(arr): 
+	smallest = arr[0] # Stores the smallest value
+	smallest_index = 0 # Stores the index of the smallest value
+	for i in range(1, len(arr)):
+		if arr[i] < smallest:
+			smallest = arr[i] 
+			smallest_index = i
+    		return smallest_index
+
+
+def selectionSort(arr):
+	newArr = []
+	for i in range(len(arr)):
+		smallest = ndSmallest(arr)
+		newArr.append(arr.pop(smallest))
+		return newArr
+
+
+print selectionSort([5, 3, 6, 2, 10])
+
